1f6fe742761bf2a4eb8e9ac028d40b02f9fa272d
Xiao Xiao <xx2004xiamen@gmail.com>
2017-03-12 17:09:08 +0900

    Initial Commit

---
+++ FOGMJPEGImageViewSwift/FOGMJPEGImageViewSwift.h
@@ -0,0 +1,19 @@
+//
+//  FOGMJPEGImageViewSwift.h
+//  FOGMJPEGImageViewSwift
+//
+//  Created by Xiao Xiao on 平成29-03-12.
+//  Copyright © 平成29年 Xiao Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FOGMJPEGImageViewSwift.
+FOUNDATION_EXPORT double FOGMJPEGImageViewSwiftVersionNumber;
+
+//! Project version string for FOGMJPEGImageViewSwift.
+FOUNDATION_EXPORT const unsigned char FOGMJPEGImageViewSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FOGMJPEGImageViewSwift/PublicHeader.h>
+
+
